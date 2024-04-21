dc999054ee7e917b98c131d7365772da73342f73
kamil.cieszkowski <cieszkens@gmail.com>
2024-04-21 19:32:44 +0200

    Add hilt dependencies

--- app/src/main/java/pl/cieszk/closetopromo/MainActivity.kt
+++ app/src/main/java/pl/cieszk/closetopromo/MainActivity.kt
@@ -10,13 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.Firebase
+import com.google.firebase.initialize
 import pl.cieszk.closetopromo.navigation.AppNavGraph
 import pl.cieszk.closetopromo.ui.theme.CloseToPromoTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Firebase.initialize(this)
             AppNavGraph()
         }
     }
